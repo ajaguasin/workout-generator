b0c7fe563afbc29517a0df420b7fe83ced88bbd2
ajaguasin <ajaguasin@gmail.com>
2019-03-01 08:11:22 -0800

    Add auth methods in AuthProvider.js

--- client/src/components/auth/Context/AuthProvider.js
+++ client/src/components/auth/Context/AuthProvider.js
@@ -1,7 +1,62 @@
 import React, { Component } from "react";
+import axios from "axios";
+import setAuthToken from "../../../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
 export const AuthrContext = React.createContext();
 
 class AuthProvider extends Component {
+    state = {
+        isAuthenticated: false,
+        user: {},
+        loading: false
+    }
+    registerUser = (userData, history) =>  {
+
+      axios
+        .post("/api/users/register", userData)
+        .then(res => history.push("/login")) // re-direct to login on successful register
+        .catch(err => console.log(err) );
+    };
+
+    loginUser = userData => {
+      axios
+        .post("/api/users/login", userData)
+        .then(res => {
+          // Save to localStorage
+          // Set token to localStorage
+          const { token } = res.data;
+          localStorage.setItem("jwtToken", token);
+          // Set token to Auth header
+          setAuthToken(token);
+          // Decode token to get user data
+          const decoded = jwt_decode(token);
+          // Set current user
+          this.setCurrentUser(decoded);
+        })
+        .catch(err => console.log(err));
+    };
+
+    setCurrentUser = decoded => {
+      this.setState({isAuthenticated: true, user: decoded})
+    };
+
+    setUserLoading = () => {
+      this.setState({loading:true})
+    };
+
+    logoutUser = () => {
+      // Remove token from local storage
+      localStorage.removeItem("jwtToken");
+      // Remove auth header for future requests
+      setAuthToken(false);
+      // Set current user to empty object {} which will set isAuthenticated to false
+      setCurrentUser({});
+    };
+
+    componentDidMount() {
+
+    }
   render() {
     return (
       <ViewerContext.Provider value={}>
